f41db81fd1ebaf67f74c84260e0cc8f1c4675fd0
RameshKChoudhary <ramesh2005choudhary@gmail.com>
2026-07-05 05:40:16 +0000

    first

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # FAST_API
 learn api
+
+
+python3 -m venv venv
+source venv/bin/activate
+pip install fastapi[all]
+uvicorn main:app --reload
+
+
+to check :https://hoppscotch.io/
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
+ 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World !!"}
+
+@app.get("/posts")
+def get_post():
+    return {"data": "ur data"}
+
+@app.post("/createpost")
+def create(payload: dict = Body(...)):
+    print(payload)
+    return {"new post": f"title: {payload['title']} , content: {payload['content']}"}
